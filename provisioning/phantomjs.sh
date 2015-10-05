6a6c29c98a85339995c59f9d2c19d9e2ba984529
Israel Santana <isantana@inerza.com>
2015-10-05 16:47:01 +0100

    Fix script

--- provisioning/phantomjs.sh
+++ provisioning/phantomjs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Ubuntu dep
-sudo apt-get install -y bzip2
+sudo apt-get install -y bzip2 libfontconfig
 
 # Phantom JS
 PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
 cd /tmp/
-curl -L https://bitbucket.org/ariya/phantomks/downloads/$PHANTOM_JS.tar.bz2 | tar -zxvf
+curl -L https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2 | tar xvjf -
 sudo mv $PHANTOM_JS /usr/local/share
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
