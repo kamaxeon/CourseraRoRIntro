f334bef354450d79dd3c5018de41e67ed4ca82ea
Israel Santana <isantana@inerza.com>
2015-10-05 15:48:11 +0100

    Add common-tools script for vagrant provisioning

---
+++ provisioning/common-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update Distro
+sudo apt-get update -y
+
+# Git
+sudo apt-get install -y git
+
+# Common packages
+sudo apt-get install -y gcc build-essential libpq-dev libssl-dev libreadline-dev libsqlite3-dev zlib1g-dev
